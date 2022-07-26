e5083412703533263b4626fda5288b7fcc503c18
Ivan <vankata294n4@gmail.com>
2022-07-26 21:56:30 +0300

    Changed settings

--- pyla/pyla/settings.py
+++ pyla/pyla/settings.py
@@ -150,11 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-BASE_DIR / "staticfiles",
-]
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    BASE_DIR / 'staticfiles',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
