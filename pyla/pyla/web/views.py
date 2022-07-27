415a612dbae49295d3a65cdbab51d176ceaea2e4
Ivan <vankata294n4@gmail.com>
2022-07-27 19:20:27 +0300

    Changed index view moto from db info to fixed

--- pyla/pyla/web/views.py
+++ pyla/pyla/web/views.py
@@ -14,7 +14,7 @@ from pyla.subscriptions.models import Subscription, Features
 
 def index(request):
     context = {
-        'moto': IndexConfigurator.objects.values_list('pyla_moto', flat=True)[0]
+        'moto': 'ONE STEP AHEAD OF THE GAME'
     }
 
     return render(request, 'web/index.html', context)
@@ -29,8 +29,8 @@ class AboutPyla(views.TemplateView):
         context = super().get_context_data(**kwargs)
         # context['pyla_description'] = AboutPylaPageConfigurator.objects.values_list('about_pyla_text', flat=True)[config]
         # context['pyla_goals'] = AboutPylaPageConfigurator.objects.values_list('goals_text', flat=True)[config]
-        context['description'] = AboutPylaTextConfigurator.objects.values_list('about_pyla_text', flat=True)[config]
-        context['goals'] = AboutPylaTextConfigurator.objects.values_list('goals_text', flat=True)[config]
+        # context['description'] = AboutPylaTextConfigurator.objects.values_list('about_pyla_text', flat=True)[config]
+        # context['goals'] = AboutPylaTextConfigurator.objects.values_list('goals_text', flat=True)[config]
         #
 
         return context
