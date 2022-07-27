ee4da370a66bd3b5745dc44eb8774f1f5e05fb1e
Ivan <vankata294n4@gmail.com>
2022-07-27 18:45:19 +0300

    Fixed tkinter error

--- pyla/pyla/settings.py
+++ pyla/pyla/settings.py
@@ -102,6 +102,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
